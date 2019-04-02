b472458cb9e868880466f96adfa62aa30864c95f
Abel Acosta <abelacosta1596@gmail.com>
2019-04-02 12:58:31 -0700

    Added Main.java and QWords.txt

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,38 @@
  * Abel Acosta
  * Saul Hernandez
  */
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.TreeMap;
+
 public class Main {
+	
+	public static Map<String, Integer> treeMap;
+	public static Map<String, Integer> hashMap;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		init();
+		
+		switch(args[0]) {
+		case "hashmap":
+			readValues(hashMap);
+		case "treemap":
+			readValues(treeMap);
+		}
+	}
+	
+	// Initialize data structures
+	public static void init() {
+		treeMap = new TreeMap<String, Integer>();
+		hashMap = new HashMap<String, Integer>();
+	}
+	
+	public static void readValues(Map<String, Integer> map) {
+		Scanner input = new Scanner("QWords.txt");
+		//TODO: Read file and store in map
+		input.close();
 	}
 
 }
