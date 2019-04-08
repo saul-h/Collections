5605063b65757311b1906f55210f7413eeb20c52
saul-h <saulhernandez217@gmail.com>
2019-04-08 12:52:18 -0500

    What I got so far

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
 	
 	public static Map<String, Integer> treeMap;
 	public static Map<String, Integer> hashMap;
+	public static long time1, time2;
 
 	public static void main(String[] args) {
 		init();
@@ -37,9 +38,34 @@ public class Main {
 	}
 	
 	public static void readValues(Map<String, Integer> map) {
-		Scanner input = new Scanner("QWords.txt");
-		//TODO: Read file and store in map
-		input.close();
+		Scanner qwords = new Scanner("QWords.txt");
+		Scanner points = new Scanner("Points.txt");
+		Integer value = 0;
+		Integer pointTotals = 0;
+		String word;
+		
+		String letters;
+		time1 = System.currentTimeMillis();
+		while (points.hasNext()) {
+			if (points.hasNextInt())
+				value = points.nextInt();
+			letters = points.nextLine();
+			
+			for (int i = 0; i < letters.length(); i++) {
+				map.put(letters.substring(i, i + 1), value);
+			}
+		}
+		time2 = System.currentTimeMillis();
+		System.out.println("Time to store point values: " + (time2 - time1));
+
+		while (qwords.hasNext()) {
+			word = qwords.nextLine();
+			for (int i = 0; i < word.length(); i++) {
+				word.substring(i, i + 1)
+			}
+		}
+		qwords.close();
+		points.close();
 	}
 
 }
